1bbbaa38699f275bd11c64cffc40337968cb72cc
Apeironce <102956925+Apeironce@users.noreply.github.com>
2024-05-20 15:10:19 +0400

    Create Task7

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,62 @@
 package src;
 
-import java.util.Scanner;
+
+import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Введите номер задания (1, 2, 3): ");
-        Scanner input = new Scanner(System.in);
-        int in = input.nextInt();
-        switch (in){
-            case 1:
-                a.hello();
-                break;
-            case 2:
-                b.calculator();
-                break;
-            case 3:
-                c.full_name();
-                break;
-            default:
-                System.out.print("Введён некорректный номер");
+    private static Connection connection = null;
+    private static String url = "jdbc:postgresql://95.213.183.180:5432/test_prod";
+    private static String username = "postgres";
+    private static String password = "fWs@Qu434";
+
+    public static void main(String[] args) throws SQLException {
+        Statement stmt = null;
+        ResultSet rs = null;
+        String sql;
+        String val = "";
+
+        try {
+            System.out.println("Открыл соединение\n");
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Задание 1:");
+            sql = "select * from employeesAge where age>20";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                System.out.printf("%s %s \n",
+                        rs.getString("employeeName"), rs.getString("age"));
+            }
+
+            System.out.println("\nЗадание 2:");
+            sql = "select departmentName, avg(salary) as averageSalary from departmentsalary group by departmentName";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                System.out.printf("%s %s \n",
+                        rs.getString("departmentName"), rs.getString("averageSalary"));
+            }
+            System.out.println("\nЗадание 3:");
+            sql = "select employeename, d.departmentname, d.location  from departmentemployee " +
+                    "join public.departmentlocation d on departmentemployee.departmentid = d.departmentid";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                System.out.printf("%s %s %s\n",
+                        rs.getString("employeename"), rs.getString("departmentname"),
+                        rs.getString("location"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        finally {
+            connection.close();
+            System.out.println("\nЗакрыл соединение");
+        }
+
     }
 }
--- src/a.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public class a {
-    public static void hello() {
-        System.out.print("Hello world");
-    }
-}
